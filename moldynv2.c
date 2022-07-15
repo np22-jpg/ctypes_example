e2f40ea9245c63483326a7f2b88c356817760920
Nolan Poe <npgo22@gmail.com>
2022-07-14 20:05:47 -0700

    do a little formatting

--- moldynv2.c
+++ moldynv2.c
@@ -1,29 +1,14 @@
-#include <math.h>
 #include <stdio.h>
 
+// a main() isn't needed because this program is now a library.
+
 void graph(double y, double v, double dt, double m) {
-  // double y, v, t, dt, F, m, g = -9.8;
   double F, g = -9.8, t=0;
-  // int j, N;
 
-  printf("%f %f %f %f",y,v,dt,m);
+  printf("%f %f %f %f",y,v,dt,m); //can't figure out how to make lldb work with libraries.
 
-  FILE* fileout;
-  fileout = fopen("output", "w");
-
-  // printf("\nEnter mass m: ");
-  // scanf("%lf", &m);
-
-  // printf("\nEnter starting height y and velocity v: ");
-  // scanf("%lf %lf", &y, &v);
-
-  // printf("\nEnter time step dt and number of steps N:    ");
-  // scanf("%lf %i", &dt,&N);
-
-  // printf("\nEnter time step dt: ");
-  // scanf("%lf", &dt);
-
-  t = 0.;
+  FILE* fileout; 
+  fileout = fopen("output", "w"); //output to a file called "output"
 
   fprintf(fileout, "\n    %12.6lf %12.6lf", t, y);
 
